9fbb9e6341b1f2983c883830e2712399258a085a
HiraGul <hira24148@gmail.com>
2024-05-23 12:15:17 +0500

    Typing method channel

---
+++ android/src/main/java/com/example/twilio_chat_conversation/Interface/TypingInterface.java
@@ -0,0 +1,7 @@
+package android.src.main.java.com.example.twilio_chat_conversation.Interface;
+
+import java.util.Map;
+
+public interface TypingInterface {
+    default void onTypingUpdate(Map status) {}
+}
