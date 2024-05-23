475530695e3c55afc3e651d284107987b71efbf8
HiraGul <hira24148@gmail.com>
2024-05-23 15:04:22 +0500

    Typing method channel

--- android/src/main/java/com/example/twilio_chat_conversation/TwilioChatConversationPlugin.java
+++ android/src/main/java/com/example/twilio_chat_conversation/TwilioChatConversationPlugin.java
@@ -20,7 +20,7 @@ import io.flutter.plugin.common.EventChannel.EventSink;
 import io.flutter.plugin.common.EventChannel.StreamHandler;
 
 /** TwilioChatConversationPlugin */
-public class TwilioChatConversationPlugin implements FlutterPlugin, MethodCallHandler , StreamHandler , MessageInterface, AccessTokenInterface {
+public class TwilioChatConversationPlugin implements FlutterPlugin, MethodCallHandler , StreamHandler , MessageInterface, AccessTokenInterface,TypingInterface {
   /// The MethodChannel that will the communication between Flutter and native Android
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
